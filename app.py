d59c68fce7fcb1ce1a32ed9a9e147da533343752
Matheus <maths.menezes@gmail.com>
2021-05-31 19:49:50 -0300

    winway

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/agecode")
 def get_code():
-    return "TJUN 200"
+    with open('vars/winway', 'rb') as f:
+        return pickle.load(f)
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5000)
