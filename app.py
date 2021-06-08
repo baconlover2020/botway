f2394d3597f42a37c6352e3a8055bbf1e6187a8d
Matheus <maths.menezes@gmail.com>
2021-06-08 19:47:00 -0300

    hog

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 import pickle
 
+
 app = Flask(__name__)
 
 @app.route("/agecode")
 def get_code():
     with open('vars/winway', 'rb') as f:
         return pickle.load(f)
+
+@app.route("/hogwarts")
+def get_names():
+    return "Steinway HWA-RPG SpaceBlythe MariettaSanchez"
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5000)
