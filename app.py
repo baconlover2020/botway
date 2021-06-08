62d573e01bd14019bcdf41a51456a97e90c52204
Matheus <maths.menezes@gmail.com>
2021-06-08 19:49:30 -0300

    hog2

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def get_code():
     with open('vars/winway', 'rb') as f:
         return pickle.load(f)
 
+@app.route("/hogwarts")
+def get_names():
+    return "Steinway HWA-RPG SpaceBlythe MariettaSanchez"
+
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=5000)
