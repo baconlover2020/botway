016d1f1b5f004257a3e1fd24de329890de9a8269
matheus <maths.menezes@gmail.com>
2021-06-22 18:22:31 +0000

    port80

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def get_names():
     return "Steinway HWA-RPG SpaceBlythe MariettaSanchez"
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+      app.run(host='0.0.0.0', port=80)
