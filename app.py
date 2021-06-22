d412f274d26928e58184b92b77751765799d8df4
Matheus <maths.menezes@gmail.com>
2021-06-22 16:27:49 -0300

    agoravai

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def get_autorizados():
     return "Steinway lolaindaeyo teresa170 Guinness Maifoy Osgar Luiza. Ranger. Tassi."
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+      app.run(host='0.0.0.0', port=80)
