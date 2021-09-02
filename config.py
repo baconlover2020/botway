bd1fe2cf394e54cd0dac7f2090cc3f46de776d10
Matheus <maths.menezes@gmail.com>
2021-09-02 18:02:41 -0300

    pushing to prod

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 dev = 'ODM4OTMwNTQ1NDg3NTExNTYz.YJCQ9A.4s-d4lMa6LPw5jXg7q1Z3Qnn4qg'
-production = 'ODExNjI0NTIwNjE5OTE3MzQ1.YC06PA.0NvEcX1dGzvIWoW7HiL0xWFW5Lo'
+prod = 'ODExNjI0NTIwNjE5OTE3MzQ1.YC06PA.0NvEcX1dGzvIWoW7HiL0xWFW5Lo'
 
-botToken = dev
+botToken = prod
 
 #Credenciais de login no painel
 username = 'Martinhamiguelangelo9'
@@ -20,14 +20,16 @@ embaixador = 'Embaixadores'
 aea = 'Equipe AeA'
 
 #Channels
-ads_emblema = 779126812541452329
+ads_emblemas = 779126812541452329
 ads_icon = 779127315640614922
 ads_mobis = 777999347920011264
 
 age_botway = 837803727485534218
+age_emblemas = 883091460797378641
 aea_botway = 834895207069122580
 
-canais_emblema = [ads_emblema]
+
+canais_emblema = [ads_emblemas, age_emblemas]
 canais_furni = [ads_mobis]
 canais_icon = [ads_icon]
 canais_buscar_mobi = [age_botway, aea_botway]
